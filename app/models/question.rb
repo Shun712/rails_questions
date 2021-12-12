782458a1081f9875907adc01876c86ac29bd2823
Shun712 <sh705vi@gmail.com>
2021-12-12 22:26:09 +0900

    質問モデルにコールバックを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
+  before_validation :set_titleless_title
+
+  private
+
+    def set_titleless_title
+      self.title = 'タイトルなし' if title.blank?
+    end
 end

423a9624ad1aa22d1b15fdd28bb9c85e1cd2966f
Shun712 <sh705vi@gmail.com>
2021-12-13 05:27:33 +0900

    管理画面用ログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :questions, only: %i[index show destroy]
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
   end
   root to: 'questions#index'
   resources :questions do

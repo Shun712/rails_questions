5b3d346fc970a004643ceb18d1cccddcfded73dd
Shun712 <sh705vi@gmail.com>
2021-12-18 22:39:59 +0900

    サンプルユーザー、質問、回答を作成

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'pry-rails'
 gem 'ransack'
 gem 'will_paginate', '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'gimei'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: '管理者',
+  nickname: 'マネージャー',
+  email: 'manager@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true
+)
+
+99.times do |n|
+  name  = Gimei.name
+  nickname = Gimei.name
+  email = "sample-#{n+1}@example.com"
+  password = "foobar"
+  User.create!(name:  name,
+               nickname: nickname,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+50.times do |n|
+  Question.create!(title: "質問タイトル#{n}",
+                   content: "質問サンプル#{n}",
+                   user_id: User.find(n+1).id,
+                   solved_check: [false, true].sample)
+end
+
+48.times do |n|
+  Answer.create!(question_id: Question.find(n+2).id,
+                 user_id: User.find(n+2).id,
+                 content: "回答#{n}")
+end
